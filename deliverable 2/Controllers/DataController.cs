0234eb9b0ccb5dfd7a2b0eec9f6ce1b844041cbf
Lesego <u18261681@tuks.co.za>
2019-09-20 10:07:05 +0200

    Deliverable 4 Created

---
+++ deliverable 2/Controllers/DataController.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace deliverable_2.Controllers
+{
+    public class DataController : Controller
+    {
+        // GET: Data
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+        public ActionResult Account()
+        {
+            return View();
+        }
+        public ActionResult EmailUpdate()
+        {
+            return View();
+        }
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+        public ActionResult MainMenu()
+        {
+            return View();
+        }
+        public ActionResult Settings()
+        {
+            return View();
+        }
+        public ActionResult SubjectContent()
+        {
+            return View();
+        }
+        public ActionResult UpdateEmail()
+        {
+            return View();
+        }
+        public ActionResult Diversity()
+        {
+            return View();
+        }
+        public ActionResult Finances()
+        {
+            return View();
+        }
+        public ActionResult Math()
+        {
+            return View();
+        }
+        public ActionResult EmailEtiquette()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View();
+        }
+        public ActionResult Rights()
+        {
+            return View();
+        }
+        public ActionResult English()
+        {
+            return View();
+        }
+        public ActionResult ComputerSkills()
+        {
+            return View();
+        }
+        public ActionResult Help()
+        {
+            return View();
+        }
+        public ActionResult DeleteAccount()
+        {
+            return View();
+        }
+        public ActionResult Quiz()
+        {
+            return View();
+        }
+        public ActionResult Activity()
+        {
+            return View();
+        }
+
+        public ActionResult PM() //check
+        {
+            return View();
+        }
+
+        public ActionResult QS() //check
+        {
+            return View();
+        }
+    }
+}
